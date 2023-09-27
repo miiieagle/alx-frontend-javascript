e144f531faa5f3d63019a6295d319a95a8435332
miieagle <miieagle06@gmail.com>
2023-09-27 10:57:53 +0000

    taskk0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,31 +1,8 @@
-// Create a constructor function or an object
-function APIHandler() {}
-
-// Define the prototype function getResponseFromAPI
-APIHandler.prototype.getResponseFromAPI = function () {
-  // Simulate an asynchronous API call
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const randomValue = Math.random();
-
-      // Simulate success (resolve) or failure (reject)
-      if (randomValue < 0.5) {
-        resolve({ data: "API response data" });
-      } else {
-        reject(new Error("API request failed"));
-      }
-    }, 1000); // Simulate a 1-second delay
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
   });
-};
+}
 
-// Create an instance of the APIHandler
-const apiHandler = new APIHandler();
-
-// Call the getResponseFromAPI function and handle the Promise
-apiHandler.getResponseFromAPI()
-  .then(response => {
-    console.log("API call successful:", response);
-  })
-  .catch(error => {
-    console.error("API call failed:", error);
-  });
+export default getFullResponseFromAPI;
